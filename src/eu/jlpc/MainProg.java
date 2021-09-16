178bd09e6e68e1c6a01dcc2ad6e78c796018a59a
HippieStereo <stereobroo@yahoo.com>
2021-09-16 20:29:14 +0100

    Update MainProg.java
    New main program before apply any solid

--- src/eu/jlpc/MainProg.java
+++ src/eu/jlpc/MainProg.java
@@ -10,31 +10,37 @@ public class MainProg {
 		
 		Person user = new Person();
 		
-		String input = "";
+		System.out.println("Hit \"q\" to quit");			
+
+		System.out.print("Enter first name : ");
 		
-		System.out.println("Hit \"q\" to quit");
+		user.firstName = scanner.nextLine();
 		
-		while(input != null && !"q".equalsIgnoreCase(input) && 
-				input.isBlank() && input.isEmpty()) {
+		System.out.print("Enter last name : ");
+		
+		user.lastName = scanner.nextLine();
+		
+		if((user.firstName == null || 
+			user.firstName.isBlank() ||
+			user.firstName.isEmpty())) {
 			
-			System.out.print("Enter first name : ");
-			
-			input = scanner.nextLine();
-			
-			user.firstName = input;
-			
-			System.out.print("Enter last name : ");
-			
-			input = scanner.nextLine();
-			
-			user.lastName = input;
+			System.out.println("You did not give us a valid first name");
 			
 		}
+
+		if((user.lastName == null || 
+				user.lastName.isBlank() ||
+				user.lastName.isEmpty())) {
+				
+			System.out.println("You did not give us a valid last name");
+				
+		}
+		
 		
 		System.out.println("Your username is : " + user.firstName.substring(0, 1)
-		+ user.lastName);
+		+ user.lastName + "\n");
 		
-		input = scanner.nextLine();
+		scanner.close();
 
 	}
 
